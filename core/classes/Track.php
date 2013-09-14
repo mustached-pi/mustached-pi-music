05da44440baea31ed688cdceb13e4d49c94a44a9
Angelo Lupo <angelolupo94@gmail.com>
2013-09-15 00:24:35 +0100

    lyrics dowloader

--- core/classes/Track.php
+++ core/classes/Track.php
@@ -1,9 +1,26 @@
 <?php
 
-class Track extends Collection {
+class Track extends Collection
+{
 	
-	public function playtime() {
+	public function playtime()
+	{
 		return gmdate("i:s", $this->playtime);
+	}
+
+	public function findLyrics()
+	{
+		$url 	= 'http://api.chartlyrics.com/apiv1.asmx/SearchLyricDirect?';
+
+		$search = 'artist='.urlencode($this->artist).'&song='.urlencode($this->title);
+		echo "Searching for {$track['title']} by {$track['artist']}... \n";
+		$p1 	= file_get_contents("$url{$search}");
+		$song   = new SimpleXMLElement($p1);
+		if (!$song->Lyric)
+		{
+			return false;
+		}
+		return $song->Lyric;
 	}
 
 }
